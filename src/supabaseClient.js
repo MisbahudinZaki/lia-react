cf4a7860001b1db7b09a31ece8d88d5fdea44911
unknown <misbahudinzaki07@gmail.com>
2026-05-05 21:57:18 +0700

    perbaikan api key data dari supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://ctwpalgixjvxcczpzala.supabase.co"
-const supabaseKey = "postgresql://postgres:[YOUR-PASSWORD]@db.ctwpalgixjvxcczpzala.supabase.co:5432/postgres"
+const supabaseKey = "sb_publishable_AnDBTlFFC3e-aRK5pVcXTw_pCLd52it"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
